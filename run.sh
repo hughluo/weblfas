b14145b7f2462b055f28b9a95add52479b16c9d6
wexort <bugwriter@outlook.com>
2020-06-30 07:49:57 +0000

    modify run

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-WEBLFAS_APP_ID=$1
-WEBLFAS_SECRET_KEY=$2
-sudo apt-get install wget
+export WEBLFAS_APP_ID=$1
+export WEBLFAS_SECRET_KEY=$2
+sudo apt-get install python3-pip -y && pip3 install -r requirements.txt
+sudo apt-get install wget -y
 wget -O target.mp3 $3
-python3 webflas.py
-python3 parser.py
+python3 weblfas.py && python3 parser.py
